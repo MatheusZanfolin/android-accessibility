984403d51c29704788f803b1157603806f4158d4
matheus.zanfolin <matheus.zanfolin@dextra-sw.com>
2019-04-12 09:10:56 -0300

    Added constrast switch implementation

--- app/src/main/java/br/com/dextra/accessibility/MainActivity.kt
+++ app/src/main/java/br/com/dextra/accessibility/MainActivity.kt
@@ -1,12 +1,58 @@
 package br.com.dextra.accessibility
 
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import br.com.dextra.accessibility.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    lateinit var defaultIntroductionColor: ColorStateList
+    lateinit var defaultTitleColor: ColorStateList
+    lateinit var defaultSwitchTextColor: ColorStateList
+    lateinit var defaultButtonTextColor: ColorStateList
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setDefaultTextColors()
+        setupSwitch()
+    }
+
+    private fun setDefaultTextColors() {
+        defaultTitleColor = binding.loginAppNameText.textColors
+        defaultIntroductionColor = binding.loginIntroductionText.textColors
+        defaultSwitchTextColor = binding.contrastSwitchText.textColors
+        defaultButtonTextColor = binding.loginButton.textColors
+    }
+
+    private fun setupSwitch() {
+        binding.contrastSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                applyConstrast()
+            } else {
+                removeContrast()
+            }
+        }
+    }
+
+    private fun applyConstrast() {
+        binding.root.background = getDrawable(R.color.backgroundDark)
+        binding.loginAppNameText.setTextColor(Color.WHITE)
+        binding.loginIntroductionText.setTextColor(Color.WHITE)
+        binding.contrastSwitchText.setTextColor(Color.WHITE)
+        binding.loginButton.setTextColor(Color.WHITE)
+    }
+
+    private fun removeContrast() {
+        binding.root.background = getDrawable(R.color.background)
+        binding.loginAppNameText.setTextColor(defaultTitleColor)
+        binding.loginIntroductionText.setTextColor(defaultIntroductionColor)
+        binding.contrastSwitchText.setTextColor(defaultSwitchTextColor)
+        binding.loginButton.setTextColor(defaultButtonTextColor)
     }
 }
