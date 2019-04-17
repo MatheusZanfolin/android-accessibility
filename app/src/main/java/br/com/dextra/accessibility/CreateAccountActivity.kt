8118e73d9fd4e0697c45b3be51b8e5f821e6f849
matheus.zanfolin <matheus.zanfolin@dextra-sw.com>
2019-04-17 09:14:24 -0300

    Added info icons layout

--- app/src/main/java/br/com/dextra/accessibility/CreateAccountActivity.kt
+++ app/src/main/java/br/com/dextra/accessibility/CreateAccountActivity.kt
@@ -3,6 +3,7 @@ package br.com.dextra.accessibility
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
